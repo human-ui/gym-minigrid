deb3bd7b7bf65858f8e5845a702f9ea844f9b045
Linas Klimas <linas@threethirds.ai>
2020-01-09 12:47:03 +0200

    Make env  output all info in visualization mode.

--- gym_minigrid/window.py
+++ gym_minigrid/window.py
@@ -90,9 +90,9 @@ class Window(QMainWindow):
         except ValueError:
             print(f'unknown action {action}')
 
-        _, _, done, _ = self.env.step(action_idx)
+        obs, reward, done, reset_mask = self.env.step(action_idx)
 
         if done:
             self.reset()
         self.render()
-        return action
+        return obs, reward, done, reset_mask
