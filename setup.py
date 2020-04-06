132713b4df85a2273c3c8ca4b2c7238cb829540c
Jonas Kubilius <qbilius@gmail.com>
2020-04-06 16:53:05 +0300

    updated dependencies

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ setup(
     version='2.0',
     keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
     url='https://github.com/maximecb/gym-minigrid',
-    description='Minimalistic gridworld package for OpenAI Gym',
+    description='Minimalistic gridworld package for OpenAI Gym with multiple environments running in paraller',
     packages=['gym_minigrid'],
     install_requires=[
         'gym>=0.9.6',
         'numpy>=1.15.0',
-        'pyqt5>=5.10.1',
         'sty'
     ]
 )
